12d920a1fc9b50aa043552b6d8623b995884a13f
DRFR0ST <DRFR0ST@users.noreply.github.com>
2019-09-14 01:03:28 +0000

    Automated deployment: Sat Sep 14 01:03:28 UTC 2019 96d21ad93429e9eed061fe00a1d89221ab9b8d8a

---
+++ precache-manifest.ff1aaf75b41c722dd394526460478943.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ee6dfb591a95aa705d8dd9bd1e9e3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "005a080c2b909a4cd19f",
+    "url": "/static/css/main.8a3e115b.chunk.css"
+  },
+  {
+    "revision": "11fb76a76dd5527a6b5c",
+    "url": "/static/js/2.70f9a63f.chunk.js"
+  },
+  {
+    "revision": "005a080c2b909a4cd19f",
+    "url": "/static/js/main.a0f2688d.chunk.js"
+  },
+  {
+    "revision": "4ea1a6cb7cd0182b0478",
+    "url": "/static/js/runtime~main.5ee5188c.js"
+  },
+  {
+    "revision": "d523bbb311461bef15a14337db833838",
+    "url": "/static/media/about.d523bbb3.svg"
+  },
+  {
+    "revision": "fbb6ab424235ca3bb78b7fcce7edf9be",
+    "url": "/static/media/api_2.fbb6ab42.svg"
+  },
+  {
+    "revision": "c40e30bb7d4b763b30e1acf844043d34",
+    "url": "/static/media/starman.c40e30bb.svg"
+  },
+  {
+    "revision": "f4f8301f2b9a47b6b4eb14339a50e0c4",
+    "url": "/static/media/undraw_elements_cipa.f4f8301f.svg"
+  }
+]);
