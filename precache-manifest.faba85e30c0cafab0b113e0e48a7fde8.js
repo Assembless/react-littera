d8a551495793379eea83a0d1101bba2de80d774e
DRFR0ST <DRFR0ST@users.noreply.github.com>
2019-11-18 19:14:30 +0000

    Automated deployment: Mon Nov 18 19:14:30 UTC 2019 7f76a678403b60600b7202a0c1c2a2940db87c90

---
+++ precache-manifest.faba85e30c0cafab0b113e0e48a7fde8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7db9ecf674e737bec0ac501eacdd98",
+    "url": "/react-littera/index.html"
+  },
+  {
+    "revision": "55263e54eb41cc1c10cd",
+    "url": "/react-littera/static/css/main.8a3e115b.chunk.css"
+  },
+  {
+    "revision": "ba737335df9bd89d1116",
+    "url": "/react-littera/static/js/2.e0065b76.chunk.js"
+  },
+  {
+    "revision": "55263e54eb41cc1c10cd",
+    "url": "/react-littera/static/js/main.0dbc9320.chunk.js"
+  },
+  {
+    "revision": "082c30ae5a1819c2795a",
+    "url": "/react-littera/static/js/runtime~main.c618e6c5.js"
+  },
+  {
+    "revision": "d523bbb311461bef15a14337db833838",
+    "url": "/react-littera/static/media/about.d523bbb3.svg"
+  },
+  {
+    "revision": "fbb6ab424235ca3bb78b7fcce7edf9be",
+    "url": "/react-littera/static/media/api_2.fbb6ab42.svg"
+  },
+  {
+    "revision": "c40e30bb7d4b763b30e1acf844043d34",
+    "url": "/react-littera/static/media/starman.c40e30bb.svg"
+  },
+  {
+    "revision": "f4f8301f2b9a47b6b4eb14339a50e0c4",
+    "url": "/react-littera/static/media/undraw_elements_cipa.f4f8301f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8fd6cf6ff739e64e395b6633b6dc95e8.js"
+  "/react-littera/precache-manifest.faba85e30c0cafab0b113e0e48a7fde8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-littera/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
