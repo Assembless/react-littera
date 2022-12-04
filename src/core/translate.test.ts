245b2a4bf547a08068a2e6810fb53454ae233bc8
Mike Eling <mike.eling97@gmail.com>
2022-12-04 03:59:20 +0100

    :white_check_mark: Improve/Add tests

--- src/core/translate.test.ts
+++ src/core/translate.test.ts
@@ -22,11 +22,20 @@ describe('translate', () => {
   it('is a function', () => {
     expect(typeof translate).toEqual('function')
   })
+
   it('returns a translated string', () => {
     expect(translate(translations, 'de_DE').example).toEqual('Beispiel')
     expect(translate(translations, 'de_DE').hello('Mike')).toEqual(
       'Hallo Mike!'
     )
     expect(translate(translations, 'de_DE').numbers[1]).toEqual('Zweiter')
+  })
+
+  it('handles nested translations', () => {
+    expect(translate(translations, 'pl_PL').numbers[1]).toEqual('Drugi')
+  })
+
+  it('handles translations with placeholders', () => {
+    expect(translate(translations, 'en_US').hello('Bob')).toEqual('Hello Bob!')
   })
 })
--- src/utils/helpers.test.ts
+++ src/utils/helpers.test.ts
@@ -1,4 +1,4 @@
-import { throwInvalidLocale } from './helpers'
+import { deepMerge, throwInvalidLocale } from './helpers'
 
 describe('throwInvalidLocale', () => {
   it('should not throw if locale is included in locales', () => {
@@ -8,5 +8,84 @@ describe('throwInvalidLocale', () => {
     expect(() => throwInvalidLocale(['en_US', 'de_DE'], 'pl_PL')).toThrowError(
       'Invalid locale provided. Expected [en_US, de_DE], got pl_PL'
     )
+  })
+})
+
+describe('deepMerge', () => {
+  test('merges two objects', () => {
+    const obj1 = {
+      a: 1,
+      b: 2,
+      c: 3
+    }
+    const obj2 = {
+      b: 4,
+      c: 5,
+      d: 6
+    }
+    const expected = {
+      a: 1,
+      b: 4,
+      c: 5,
+      d: 6
+    }
+    expect(deepMerge(obj1, obj2)).toEqual(expected)
+  })
+
+  test('merges multiple objects', () => {
+    const obj1 = {
+      a: 1,
+      b: 2,
+      c: 3
+    }
+    const obj2 = {
+      b: 4,
+      c: 5,
+      d: 6
+    }
+    const obj3 = {
+      d: 7,
+      e: 8,
+      f: 9
+    }
+    const expected = {
+      a: 1,
+      b: 4,
+      c: 5,
+      d: 7,
+      e: 8,
+      f: 9
+    }
+    expect(deepMerge(obj1, obj2, obj3)).toEqual(expected)
+  })
+
+  test('handles nested objects', () => {
+    const obj1 = {
+      a: 1,
+      b: 2,
+      c: {
+        d: 3,
+        e: 4
+      }
+    }
+    const obj2 = {
+      b: 5,
+      c: {
+        e: 6,
+        f: 7
+      },
+      g: 8
+    }
+    const expected = {
+      a: 1,
+      b: 5,
+      c: {
+        d: 3,
+        e: 6,
+        f: 7
+      },
+      g: 8
+    }
+    expect(deepMerge(obj1, obj2)).toEqual(expected)
   })
 })
